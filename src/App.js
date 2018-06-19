43c986cb4bbd54d5363112b4139e981e51cb2112
Unknown <guillem.lion.benosa@gmail.com>
2018-06-19 21:20:22 +0200

    Removed Etherscan for web3

--- src/App.js
+++ src/App.js
@@ -5,28 +5,13 @@ import { css } from "glamor";
 
 import Web3 from "web3";
 
-import Emojify from "react-emojione";
+import Leaderboard from "./Leaderboard/Leaderboard";
 
 const donationNetworkID = 1; // make sure donations only go through on this network.
 
 const donationAddress = "0x5adf43dd006c6c36506e2b2dfa352e60002d22dc"; //replace with the address to watch
-const apiKey = "6DIUB7X6S92YJR6KXKF8V8ZU55IXT5PN2S"; //replace with your own key
-
-const etherscanApiLinks = {
-  extTx:
-    "https://api.etherscan.io/api?module=account&action=txlistinternal&address=" +
-    donationAddress +
-    "&startblock=0&endblock=99999999&sort=asc&apikey=" +
-    apiKey,
-  intTx:
-    "https://api.etherscan.io/api?module=account&action=txlist&address=" +
-    donationAddress +
-    "&startblock=0&endblock=99999999&sort=asc&apikey=" +
-    apiKey
-};
-
-const isSearched = searchTerm => item =>
-  item.from.toLowerCase().includes(searchTerm.toLowerCase());
+const deploymentBlock = "4448139"; // Passing a deployment block significantly speeds up the loading process
+const leaderboardType = "leaderboard"; // can be a "leaderboard" or "multisig"
 
 var myweb3;
 
@@ -35,11 +20,9 @@ class App extends Component {
     super(props);
 
     this.state = {
-      ethlist: [],
       searchTerm: "",
       donateenabled: true,
-      socketconnected: false,
-      totalAmount: 0
+      socketconnected: false
     };
   }
 
@@ -47,83 +30,6 @@ class App extends Component {
     this.setState({
       searchTerm: event.target.value
     });
-  };
-
-  subscribe = address => {
-    let ws = new WebSocket("wss://socket.etherscan.io/wshandler");
-
-    function pinger(ws) {
-      var timer = setInterval(function() {
-        if (ws.readyState === 1) {
-          ws.send(
-            JSON.stringify({
-              event: "ping"
-            })
-          );
-        }
-      }, 20000);
-      return {
-        stop: function() {
-          clearInterval(timer);
-        }
-      };
-    }
-
-    ws.onopen = function() {
-      this.setState({
-        socketconnected: true
-      });
-      pinger(ws);
-      ws.send(
-        JSON.stringify({
-          event: "txlist",
-          address: address
-        })
-      );
-    }.bind(this);
-    ws.onmessage = function(evt) {
-      let eventData = JSON.parse(evt.data);
-      console.log(eventData);
-      if (eventData.event === "txlist") {
-        let newTransactionsArray = this.state.transactionsArray.concat(
-          eventData.result
-        );
-        this.setState(
-          {
-            transactionsArray: newTransactionsArray
-          },
-          () => {
-            this.processEthList(newTransactionsArray);
-          }
-        );
-      }
-    }.bind(this);
-    ws.onerror = function(evt) {
-      this.setState({
-        socketerror: evt.message,
-        socketconnected: false
-      });
-    }.bind(this);
-    ws.onclose = function() {
-      this.setState({
-        socketerror: "socket closed",
-        socketconnected: false
-      });
-    }.bind(this);
-  };
-
-  getAccountData = () => {
-    let fetchCalls = [
-      fetch(`${etherscanApiLinks.extTx}`),
-      fetch(`${etherscanApiLinks.intTx}`)
-    ];
-    return Promise.all(fetchCalls)
-      .then(res => {
-        return Promise.all(res.map(apiCall => apiCall.json()));
-      })
-      .then(responseJson => {
-        return [].concat.apply(...responseJson.map(res => res.result));
-      });
   };
 
   handleDonate = event => {
@@ -178,62 +84,6 @@ class App extends Component {
     });
   };
 
-  processEthList = ethlist => {
-    // let totalAmount = new myweb3.utils.BN(0);
-    let filteredEthList = ethlist
-      .map(obj => {
-        obj.value = new myweb3.utils.BN(obj.value); // convert string to BigNumber
-        return obj;
-      })
-      .filter(obj => {
-        return obj.value.cmp(new myweb3.utils.BN(0));
-      }) // filter out zero-value transactions
-      .reduce((acc, cur) => {
-        // group by address and sum tx value
-        if (cur.isError !== "0") {
-          // tx was not successful - skip it.
-          return acc;
-        }
-        if (cur.from === donationAddress) {
-          // tx was outgoing - don't add it in
-          return acc;
-        }
-        if (typeof acc[cur.from] === "undefined") {
-          acc[cur.from] = {
-            from: cur.from,
-            value: new myweb3.utils.BN(0),
-            input: cur.input,
-            hash: []
-          };
-        }
-        acc[cur.from].value = cur.value.add(acc[cur.from].value);
-        acc[cur.from].input =
-          cur.input !== "0x" && cur.input !== "0x00"
-            ? cur.input
-            : acc[cur.from].input;
-        acc[cur.from].hash.push(cur.hash);
-        return acc;
-      }, {});
-    filteredEthList = Object.keys(filteredEthList)
-      .map(val => filteredEthList[val])
-      .sort((a, b) => {
-        // sort greatest to least
-        return b.value.cmp(a.value);
-      })
-      .map((obj, index) => {
-        // add rank
-        obj.rank = index + 1;
-        return obj;
-      });
-    const ethTotal = filteredEthList.reduce((acc, cur) => {
-      return acc.add(cur.value);
-    }, new myweb3.utils.BN(0));
-    return this.setState({
-      ethlist: filteredEthList,
-      totalAmount: parseFloat(myweb3.utils.fromWei(ethTotal)).toFixed(2)
-    });
-  };
-
   componentDidMount = () => {
     if (
       typeof window.web3 !== "undefined" &&
@@ -251,18 +101,6 @@ class App extends Component {
       });
       myweb3 = new Web3();
     }
-
-    this.getAccountData().then(res => {
-      this.setState(
-        {
-          transactionsArray: res
-        },
-        () => {
-          this.processEthList(res);
-          this.subscribe(donationAddress);
-        }
-      );
-    });
   };
 
   render = () => {
@@ -382,46 +220,11 @@ class App extends Component {
           </div>
         </div>
 
-        <div className="flex-column leaderboard">
-          <table className="table">
-            <thead className="pagination-centered">
-              <tr>
-                <th>Rank</th>
-                <th>Address</th>
-                <th>Value</th>
-                <th>Message</th>
-                <th>Tx Link</th>
-              </tr>
-            </thead>
-            <tbody>
-              {this.state.ethlist
-                .filter(isSearched(this.state.searchTerm))
-                .map(item => (
-                  <tr key={item.hash} className="Entry">
-                    <td>{item.rank} </td>
-                    <td>{item.from} </td>
-                    <td>{myweb3.utils.fromWei(item.value)} ETH</td>
-                    <td>
-                      <Emojify>
-                        {item.input.length &&
-                          myweb3.utils.hexToAscii(item.input)}
-                      </Emojify>
-                    </td>
-                    <td className="table-tx-header">
-                      {item.hash.map((txHash, index) => (
-                        <a
-                          key={index}
-                          href={"https://etherscan.io/tx/" + txHash}
-                        >
-                          [{index + 1}]
-                        </a>
-                      ))}
-                    </td>
-                  </tr>
-                ))}
-            </tbody>
-          </table>
-        </div>
+        <Leaderboard
+          address={donationAddress}
+          deploymentBlock={deploymentBlock}
+          type={leaderboardType}
+        />
       </div>
     );
   }; // End of render()
